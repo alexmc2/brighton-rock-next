debce18ef2db55e9a99f5d114be0686ddc617b89
Alex <alex_mcgarry@hotmail.co.uk>
2024-06-19 19:28:43 +0100

    initial commit

---
+++ app/(site)/page.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import Hero from '@/components/Hero';
+import AboutSection from '@/components/About';
+import CooperativeSection from '@/components/CooperativeSection';
+import Gallery from '@/components/Gallery';
+import { sanityFetch } from '../client-config';
+import { SanityDocument } from 'next-sanity';
+
+const HOME_QUERY = `*[_type == "home"][0]{
+  aboutTitle,
+  aboutContent,
+  cooperativeTitle,
+  cooperativeContent,
+  galleryTitle,
+  galleryImages
+}`;
+
+export default async function HomePage() {
+  const homePageContent = await sanityFetch<SanityDocument>({
+    query: HOME_QUERY,
+  });
+  console.log('Home Page Content:', homePageContent);
+
+  return (
+    <div>
+      <Hero
+        title="Welcome to Brighton Rock"
+        description="A small housing co-op established in 1987, located in West Hove"
+        showLogo={true}
+      />
+      {homePageContent ? (
+        <>
+          <AboutSection
+            title={homePageContent.aboutTitle}
+            content={homePageContent.aboutContent}
+          />
+          <CooperativeSection
+            title={homePageContent.cooperativeTitle}
+            content={homePageContent.cooperativeContent}
+          />
+          <Gallery
+            title={homePageContent.galleryTitle}
+            images={homePageContent.galleryImages}
+          />
+        </>
+      ) : (
+        <div>Error loading home page content.</div>
+      )}
+    </div>
+  );
+}
